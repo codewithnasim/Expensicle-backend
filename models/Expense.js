6ea919789da217177f1f69f12c3642e3eae7aca0
NASIM ISLAM <srs.nasim@gmail.com>
2025-05-26 10:41:44 +0530

    add

--- models/Expense.js
+++ models/Expense.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
 
-const ExpenseSchema = new mongoose.Schema({
-  description: { type: String, required: true },
-  amount: { type: Number, required: true },
-  date: { type: Date, required: true },
+const expenseSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }, // Reference to the User model
+  type: { type: String, enum: ["income", "expense"], required: true }, // Type of transaction
+  title: { type: String, required: true }, // Title of the transaction
+  amount: { type: Number, required: true }, // Amount of the transaction
+  category: { type: String, required: true }, // Category of the transaction
+  date: { type: Date, required: true }, // Date of the transaction
+  notes: { type: String }, // Optional notes
 });
 
-module.exports = mongoose.model("Expense", ExpenseSchema);
+module.exports = mongoose.model("Expense", expenseSchema);
